0762756b4dee8629e95a0fff407403827866c937
Shota-Yamauchi <syota.learning@gmail.com>
2024-09-26 13:32:49 +0900

    finish reviewing ner

--- review/model.py
+++ review/model.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 from mymodels import MyRNN, MyUGRNN, MyGRU, MyLSTM 
 
 class Model(nn.Module):
+    """
+    自分で実装したクラスをインポートして使用する
+    """
     
     def __init__(self, vocab_size, embedding_dim, hidden_size, output_size, rnn_type='LSTM', bidirection=False):
         super().__init__()
@@ -44,7 +47,53 @@ class Model(nn.Module):
             out = self.fc(output_seq)
 
             return out, h_n
+
+class PytorchModel(nn.Module):
+    """
+    pytorchで実装されたクラスを使用する
+    """
+    def __init__(self, vovab_size, embedding_dim, hidden_size, output_size, rnn_type='LSTM', bidirectional=False, num_layers=1):
+        super().__init__()
+        self.num_direction = 2 if bidirectional == True else 1
+
+        # embedding layer
+        self.embedding = nn.Embedding(vovab_size, embedding_dim, padding_idx=0)
+
+        if rnn_type == 'RNN':
+            self.rnn = nn.RNN(embedding_dim, hidden_size, batch_first=True, bidirectional=bidirectional, num_layers=num_layers)
         
+        if rnn_type == 'GRU':
+            self.rnn = nn.GRU(embedding_dim, hidden_size, batch_first=True, bidirectional=bidirectional, num_layers=num_layers)
+        
+        if rnn_type == 'LSTM':
+            self.rnn = nn.LSTM(embedding_dim, hidden_size, batch_first=True, bidirectional=bidirectional, num_layers=num_layers)
+
+        else:
+            raise ValueError('Unsupported RNN type. Choose from ["LSTM", "RNN", "GRU"]')        
+        
+        # 最終層の全結合層
+        self.fc = nn.Linear(hidden_size*self.num_direction, output_size)
+    
+    def forward(self, input, h_0=None, c_0=None):
+        
+        # embedding
+        embedded_input = self.embedding(input)
+
+        if c_0 is None:
+            output_seq, _ = self.rnn(embedded_input, h_0)
+        
+        else:
+            output_seq, _ = self.rnn(embedded_input, h_0, c_0)
+        
+        out = self.fc(output_seq)
+
+        return out, _
+
+
+
+
+
+    
 
 
 
